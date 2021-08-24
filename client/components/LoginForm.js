d3b9f8cc4eb11f23a7e9b865030f21449f529c3a
Jesse Quijano <jesse@quijano.net>
2021-08-24 15:37:47 -0700

    completed login form

---
+++ client/components/LoginForm.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { graphql } from 'react-apollo';
+import { hashHistory } from 'react-router';
+import AuthForm from "./AuthForm";
+import mutation from '../mutations/Login';
+import query from '../queries/CurrentUser';
+
+class LoginForm extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { errors: [] };
+    }
+
+    componentWillUpdate(nextProps) {
+        if (!this.props.data.user && nextProps.data.user) {
+            hashHistory.push('/dashboard');
+        }
+    }
+    
+    handleSubmit = ({email, password}) => {
+        this.props.mutate({
+            variables: { email, password },
+            refetchQueries: [{ query }]
+        }).catch((res) => {
+            const errors = res.graphQLErrors.map(error => error.message);
+            this.setState({ errors });
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <h3>Login</h3>
+                <AuthForm onSubmit={this.handleSubmit} errors={this.state.errors} />
+            </div>
+        );
+    }
+}
+
+export default graphql(query)(
+    graphql(mutation)(LoginForm)
+);
